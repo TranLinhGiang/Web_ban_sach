cbf7c8ab53f7013f4000ecf35f91bbd517f68e5b
Trần Linh Giang <gt07839@gmail.com>
2024-05-20 08:54:12 +0700

    cau hinh

---
+++ web_ban_sach_backend/src/main/java/com/example/web_ban_sach_backend/WebBanSachBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.web_ban_sach_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBanSachBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebBanSachBackendApplication.class, args);
+    }
+
+}
